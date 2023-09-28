b32190d239d0b26000bb275c176d2a26a98dd174
Zohaib <zohaibrajan@gmail.com>
2023-09-28 19:39:01 -0400

    Added User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -39,6 +39,12 @@ router.post('/', async (req, res, next) => {
     return res.json({
         user: safeUser
     })
-})
+});
+
+router.delete( '/', (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+  }
+);
 
 module.exports = router;
