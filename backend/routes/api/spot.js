653ad0084f5053c31a478d8d0593c47b6f1b74b4
Zohaib <zohaibrajan@gmail.com>
2023-10-01 19:53:14 -0400

    test live_env

--- backend/routes/api/spot.js
+++ backend/routes/api/spot.js
@@ -2,38 +2,78 @@ const router = require('express').Router();
 const { Spot, SpotImage, Review } = require('../../db/models');
 const sequelize = require('sequelize')
 
+// router.get('/', async (req, res, next) => {
+//   const spots = await Spot.findAll({
+//     include: [
+//       {
+//         model: SpotImage,
+//         attributes: ['url']
+//       },
+//       {
+//         model: Review,
+//         attributes: []
+//       }
+//     ],
+//       attributes: {
+//           include: [
+//           [sequelize.fn('AVG', sequelize.col('Reviews.stars')), 'avgRating']
+//       ]
+//   },
+//     group: ['Spot.id']
+//   });
+
+//   const spotsJSON = spots.map(spot => spot.toJSON());
+
+//   for (let i = 0; i < spotsJSON.length; i++) {
+//     const spot = spotsJSON[i];
+
+//     spot.previewImage = spot.SpotImages[0].url
+//     delete spot.SpotImages
+//   }
+
+//   res.json({
+//     Spots: spotsJSON
+//   });
+// })
+
 router.get('/', async (req, res, next) => {
-  const spots = await Spot.findAll({
-    include: [
-      {
-        model: SpotImage,
-        attributes: ['url']
-      },
-      {
-        model: Review,
-        attributes: []
-      }
-    ],
-      attributes: {
-          include: [
-          [sequelize.fn('AVG', sequelize.col('Reviews.stars')), 'avgRating']
-      ]
-  },
-    group: ['Spot.id'] 
-  });
+    const spots = await Spot.findAll({
+        include: [
+            {
+                model: SpotImage,
+                attributes: ['url']
+            }
+        ]
+    });
 
-  const spotsJSON = spots.map(spot => spot.toJSON());
 
-  for (let i = 0; i < spotsJSON.length; i++) {
-    const spot = spotsJSON[i];
+    const spotsJSON = spots.map(spot => spot.toJSON());
 
-    spot.previewImage = spot.SpotImages[0].url
-    delete spot.SpotImages
-  }
+    for (let i = 0; i < spotsJSON.length; i++) {
+        const spot = spotsJSON[i];
 
-  res.json({
-    Spots: spotsJSON
-  });
+        const reviews = await Review.findAll({
+            where: {
+                spotId: spot.id
+            }
+        })
+
+        const avg = await Review.sum('stars', {
+            where: {
+                spotId: spot.id
+            }
+        }) / reviews.length;
+
+        spot.avgRating = avg
+        spot.previewImage = spot.SpotImages[0].url
+        delete spot.SpotImages
+    }
+
+    res.json({
+        Spots: spotsJSON
+    });
 })
+
+module.exports = router
 
 module.exports = router
