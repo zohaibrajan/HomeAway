f6300ec32f0b54c46dae941e1e48730f4e830c0c
Zohaib <zohaibrajan@gmail.com>
2023-10-02 14:38:57 -0400

    Removed an Endpoint tester

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -14,17 +14,5 @@ router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
 });
 
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
-router.get('/set-token-cookie', async (_req, res) => {
-  const user = await User.findOne({
-    where: {
-      username: 'Demo-lition'
-    }
-  });
-  setTokenCookie(res, user);
-  return res.json({ user: user });
-});
-
 
 module.exports = router;
