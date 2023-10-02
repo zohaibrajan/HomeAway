88153ee6a2ce0872d34f6a5c2eafbeea3b212d59
Zohaib <zohaibrajan@gmail.com>
2023-10-01 20:05:48 -0400

    final changes to the first endpoint

--- backend/routes/api/spot.js
+++ backend/routes/api/spot.js
@@ -2,40 +2,6 @@ const router = require('express').Router();
 const { Spot, SpotImage, Review } = require('../../db/models');
 const sequelize = require('sequelize')
 
-// router.get('/', async (req, res, next) => {
-//   const spots = await Spot.findAll({
-//     include: [
-//       {
-//         model: SpotImage,
-//         attributes: ['url']
-//       },
-//       {
-//         model: Review,
-//         attributes: []
-//       }
-//     ],
-//       attributes: {
-//           include: [
-//           [sequelize.fn('AVG', sequelize.col('Reviews.stars')), 'avgRating']
-//       ]
-//   },
-//     group: ['Spot.id']
-//   });
-
-//   const spotsJSON = spots.map(spot => spot.toJSON());
-
-//   for (let i = 0; i < spotsJSON.length; i++) {
-//     const spot = spotsJSON[i];
-
-//     spot.previewImage = spot.SpotImages[0].url
-//     delete spot.SpotImages
-//   }
-
-//   res.json({
-//     Spots: spotsJSON
-//   });
-// })
-
 router.get('/', async (req, res, next) => {
     const spots = await Spot.findAll({
         include: [
@@ -73,7 +39,5 @@ router.get('/', async (req, res, next) => {
         Spots: spotsJSON
     });
 })
-
-module.exports = router
 
 module.exports = router
