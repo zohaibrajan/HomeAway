7a9813e49c625b87e498b96d4754bd82d652d101
Zohaib <zohaibrajan@gmail.com>
2023-10-03 14:07:29 -0400

    Complete GET all Reviews

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,13 +2,15 @@ const router = require('express').Router();
 const sessionRouter = require('./session');
 const spotRouter = require('./spot');
 const userRouter = require('./users');
+const reviewRouter = require('./review');
 const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
 
 router.use('/session', sessionRouter);
 router.use('/users', userRouter);
-router.use('/spots', spotRouter)
+router.use('/spots', spotRouter);
+router.use('/reviews', reviewRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
