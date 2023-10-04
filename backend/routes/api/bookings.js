2c0cf334c2e2b8fbbd883b35cf235675c7fea973
Zohaib <zohaibrajan@gmail.com>
2023-10-03 22:20:02 -0400

    First booking route

---
+++ backend/routes/api/bookings.js
@@ -0,0 +1,64 @@
+const router = require('express').Router();
+const { Spot, SpotImage, Review, User, ReviewImage, Booking } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth');
+const { validateReview, validateSpot } = require('../../utils/instanceValidators')
+const sequelize = require('sequelize');
+const review = require('../../db/models/review');
+
+
+
+router.get('/current', requireAuth, async(req, res, next) => {
+    const { user } = req;
+
+    const bookings = await Booking.unscoped().findAll({
+        where: {
+            userId: user.id
+        }
+    });
+
+    const bookingsJSON = bookings.map(booking => booking.toJSON())
+
+    for (let i = 0; i < bookingsJSON.length; i++) {
+        const booking = bookingsJSON[i]
+
+        const spot = await Spot.findByPk(booking.spotId, {
+            include: [
+                {
+                    model: SpotImage,
+                    attributes: ['url', 'preview']
+                }
+            ],
+            attributes: {
+                exclude: ['description']
+            }
+        })
+
+        const spotJSON = spot.toJSON();
+
+        if (spotJSON.SpotImages.length > 0) {
+            for (let i = 0; i < spotJSON.SpotImages.length; i++) {
+                const image = spotJSON.SpotImages[i];
+
+                if (image.preview) {
+                    spotJSON.previewImage = image.url
+                    break
+                } else if (image.preview === false) {
+                    spotJSON.previewImage = null
+                }
+            }
+        } else {
+            spotJSON.previewImage = null
+        }
+
+        delete spotJSON.SpotImages
+
+        booking.Spot = spotJSON;
+
+    }
+
+    res.json({
+        Bookings: bookingsJSON
+    })
+})
+
+module.exports = router
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,6 +3,7 @@ const sessionRouter = require('./session');
 const spotRouter = require('./spot');
 const userRouter = require('./users');
 const reviewRouter = require('./review');
+const bookingRouter = require('./bookings')
 const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
@@ -11,6 +12,7 @@ router.use('/session', sessionRouter);
 router.use('/users', userRouter);
 router.use('/spots', spotRouter);
 router.use('/reviews', reviewRouter);
+router.use('/bookings', bookingRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
