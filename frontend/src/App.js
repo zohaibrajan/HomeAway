4940c80f09fd813a52d8c557bfa80e1b0ac953b9
Zohaib <zohaibrajan@gmail.com>
2023-10-20 12:44:16 -0400

    Signup Modal Complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { Route, Switch } from "react-router-dom";
-// import LoginFormPage from "./components/LoginFormPage";
-import SignupFormPage from "./components/SignUpFormPage"
+import { Switch } from "react-router-dom";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 
@@ -15,17 +13,8 @@ function App() {
 
   return (
     <>
-    <Navigation isLoaded={isLoaded} />
-    {isLoaded && (
-      <Switch>
-        {/* <Route path="/login">
-          <LoginFormPage />
-        </Route> */}
-        <Route path="/signup">
-          <SignupFormPage />
-        </Route>
-      </Switch>
-    )}
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && <Switch></Switch>}
     </>
   );
 }
