f4d83b832dc143c0d410e612074f2f847d091f52
Zohaib <zohaibrajan@gmail.com>
2023-10-26 23:00:32 -0400

    Complete

--- frontend/src/components/DeleteASpotModal/index.js
+++ frontend/src/components/DeleteASpotModal/index.js
@@ -1,33 +1,34 @@
 import React from "react";
-import './DeleteASpot.css'
+import "./DeleteASpot.css";
 import { useModal } from "../../context/Modal";
 import { useDispatch } from "react-redux";
 import { deleteASpotThunk } from "../../store/spots";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
-
 function DeleteASpotModal({ spot }) {
-    const history = useHistory()
-    const dispatch = useDispatch();
-    const { closeModal } = useModal();
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const { closeModal } = useModal();
 
-    const deleteSpot = (e) => {
-        e.preventDefault();
+  const deleteSpot = (e) => {
+    e.preventDefault();
 
-        try {
-            dispatch(deleteASpotThunk(spot.id)).then(closeModal())
-            history.push("/spots/current");
-        } catch (e) {
-            console.log(e)
-        }
-    }
+    dispatch(deleteASpotThunk(spot.id)).then(closeModal());
+    history.push("/spots/current");
+  };
 
   return (
     <div className="delete-spot-modal">
       <h1>Confirm Delete</h1>
-      <span id="delete-spot-span">Are you sure you want to remove this spot from the listings?</span>
-      <button className="confirm-delete" onClick={deleteSpot}>Yes (Delete Spot)</button>
-      <button className="do-not-delete" onClick={closeModal}>No (Keep Spot)</button>
+      <span id="delete-spot-span">
+        Are you sure you want to remove this spot from the listings?
+      </span>
+      <button className="confirm-delete" onClick={deleteSpot}>
+        Yes (Delete Spot)
+      </button>
+      <button className="do-not-delete" onClick={closeModal}>
+        No (Keep Spot)
+      </button>
     </div>
   );
 }
